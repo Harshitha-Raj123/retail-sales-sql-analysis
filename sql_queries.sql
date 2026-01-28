6859c71689b9c18f8a61183f78102522fb33a948
Harshitha Rajashekar <harshithargowda97@gmail.com>
2026-01-28 14:26:49 +0000

    Add SQL query: revenue by country

--- sql_queries.sql
+++ sql_queries.sql
@@ -3,4 +3,11 @@
 SELECT 
     SUM(Quantity * UnitPrice) AS total_revenue
 FROM data_sample;
+-- 2. Revenue by country
+SELECT 
+    Country,
+    SUM(Quantity * UnitPrice) AS total_revenue
+FROM data_sample
+GROUP BY Country
+ORDER BY total_revenue DESC;
 
